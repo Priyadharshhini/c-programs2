8301fa5816c9d4e7e53a683338e6a76f999ab371
Priyadharshhini <priyadhans2007@gmail.com>
2026-02-11 22:11:22 +0530

    Add files via upload

---
+++ counteven.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int even_count=0;
+    scanf("%d",&n);
+    for (;n!=0;n/=10){
+
+            even_count++;
+        even_count/2==0;
+    }
+    printf("%d",even_count/2==0);
+}
